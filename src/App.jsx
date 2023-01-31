711477a783780c83dcf554e0502f83d62c959950
Anshul Kulkarni <anshulkulkarni00@gmail.com>
2023-01-31 16:40:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Login from "./Pages/Login/Login"
+
+const App = () => {
+  // Make routes here 
+  // Reuse Components as much as possible 
+  // Put meaningful comments in code 
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './Login.styled'
+
+const Login = () => {
+  return (
+    <Container>
+        <h2>Login Page</h2>
+    </Container>
+  )
+}
+
+export default Login
